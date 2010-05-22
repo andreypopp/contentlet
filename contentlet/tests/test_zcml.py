7e851de7717ef997c0ba457251abe6c884d00b7a
Andrey Popp <8mayday@gmail.com>
2010-05-22 19:44:41 +0400

    Added `contentprovider` ZCML directive.

---
+++ contentlet/tests/test_zcml.py
@@ -0,0 +1,91 @@
+""" Tests for contentlet.zcml."""
+
+import unittest
+
+from zope.interface import implements
+
+from repoze.bfg import testing
+from repoze.bfg.registry import Registry
+
+from contentlet.interfaces import IContentProvider
+
+__all__ = []
+
+
+class TestContentProviderDirective(unittest.TestCase):
+
+    def setUp(self):
+        self.registry = Registry()
+        testing.setUp(registry=self.registry)
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def _callContentProvider(self, *args, **kwargs):
+        from contentlet.zcml import contentprovider
+        return contentprovider(*args, **kwargs)
+
+    def _getProvider(self, name, context_iface=None):
+        if context_iface is None:
+            from zope.interface import Interface
+            context_iface = Interface
+        return self.registry.adapters.lookup(
+            (context_iface,), IContentProvider, name=name, default=None)
+
+    def test_contentprovider_no_context(self):
+        context = DummyContext()
+        self._callContentProvider(context, DummyProvider("content"), "name")
+        self.assertEqual(len(context.actions), 1)
+        action = context.actions[0]
+        self.assertEqual(action["discriminator"], ("name",))
+        register = action["callable"]
+        register()
+        provider = self._getProvider("name")
+        self.assertNotEqual(provider, None)
+        self.assertTrue(IContentProvider.providedBy(provider))
+        self.assertEqual(provider(None, None), "content")
+
+    def test_contentprovider_no_context(self):
+        context = DummyContext()
+        self._callContentProvider(context, DummyProvider("content"), "name",
+                                 context=DummyContext)
+        self.assertEqual(len(context.actions), 1)
+        action = context.actions[0]
+        self.assertEqual(action["discriminator"], ("name",))
+        register = action["callable"]
+        register()
+        provider = self._getProvider("name")
+        self.assertEqual(provider, None)
+        from zope.interface import implementedBy
+        provider = self._getProvider("name",
+                                     context_iface=implementedBy(DummyContext))
+        self.assertNotEqual(provider, None)
+        self.assertTrue(IContentProvider.providedBy(provider))
+        self.assertEqual(provider(None, None), "content")
+
+
+class DummyProvider(object):
+    implements(IContentProvider)
+
+    def __init__(self, content):
+        self.content = content
+
+    def __call__(self, context, request):
+        return self.content
+
+
+class DummyContext(object):
+    pass
+
+
+class DummyContext(object):
+    def __init__(self):
+        self.actions = []
+
+    def action(self, discriminator, callable=None, args=(), kw={}, order=0):
+        self.actions.append(
+            {'discriminator':discriminator,
+             'callable':callable,
+             'args':args,
+             'kw':kw}
+            )
---
+++ contentlet/zcml.py
@@ -0,0 +1,40 @@
+""" ZCML directives."""
+
+from zope.interface import Interface
+from zope.configuration.fields import GlobalObject
+from zope.schema import TextLine
+
+from repoze.bfg.threadlocal import get_current_registry
+
+from contentlet.configuration import Configuration
+
+__all__ = ["IContentProviderDirective",
+           "contentprovider"]
+
+
+class IContentProviderDirective(Interface):
+
+    provider = GlobalObject(
+        title=u"Content provider to be registered.",
+        required=True)
+
+    name = TextLine(
+        title=u"The name of the content provider.",
+        required=True)
+
+    context = GlobalObject(
+        title=u"Context type or interface for which content provider register.",
+        required=False)
+
+
+def contentprovider(_context, provider=None, name=None, context=None):
+    registry = get_current_registry()
+
+    def register():
+        config = Configuration(registry)
+        config.add_content_provider(provider, name, context=context)
+
+    _context.action(
+        discriminator=(name,),
+        callable=register
+    )
