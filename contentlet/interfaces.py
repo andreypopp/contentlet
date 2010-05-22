c1675d7b4b8c691ee726dbee51a4249c43ded72d
Andrey Popp <8mayday@gmail.com>
2010-05-22 19:15:48 +0400

    Removed unused interfaces.

--- contentlet/interfaces.py
+++ contentlet/interfaces.py
@@ -1,11 +1,8 @@
 """ Interfaces."""
 
 from zope.interface import Interface
-from zope.interface.common.mapping import IReadMapping
 
-__all__ = ["IContentProvider",
-           "IViewlet",
-           "IViewletManager"]
+__all__ = ["IContentProvider"]
 
 
 class IContentProvider(Interface):
@@ -16,18 +13,3 @@ class IContentProvider(Interface):
 
     def __call__(context, request):
         """ Provide content."""
-
-
-class IViewlet(IContentProvider):
-    """ Viewlet.
-
-    Viewlet is a content provider that is managed by viewlet manager.
-    """
-
-
-class IViewletManager(IContentProvider, IReadMapping):
-    """ Viewlet manager.
-
-    Viewlet is a content provider that can manage other content providers that
-    are called viewlets.
-    """
