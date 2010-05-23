524c381c58271844a6e53aaeec517e70843647b3
Andrey Popp <8mayday@gmail.com>
2010-05-23 16:19:22 +0400

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-version = "0.1"
+version = "0.1.1"
 long_description = open("README").read() + "\n" + open("CHANGES").read()
 description = "Framework for reusable and composable UI with repoze.bfg."
 requires = []
