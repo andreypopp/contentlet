e478d3cba1a33f46f043357a503bb64dd569e66c
Andrey Popp <8mayday@gmail.com>
2010-05-23 16:17:47 +0400

    Added top level import for ContentletConfiguratorMixin.

--- contentlet/__init__.py
+++ contentlet/__init__.py
@@ -1,5 +1,6 @@
 """ Package."""
 
 from contentlet.configuration import Configurator
+from contentlet.configuration import ContentletConfiguratorMixin
 from contentlet.provider import get_provider
 from contentlet.provider import query_provider
