bf9054499a553bc8dfa8a3b2108d64d563c430b9
Andrey Popp <8mayday@gmail.com>
2010-06-02 13:09:13 +0400

    More docstrings in contentlet.provider.

--- contentlet/provider.py
+++ contentlet/provider.py
@@ -1,4 +1,4 @@
-""" Implementations of IContentProvider."""
+""" Content provider infrastructure."""
 
 from zope.interface import providedBy
 from zope.component import getGlobalSiteManager
@@ -10,7 +10,12 @@ __all__ = ["query_provider",
 
 
 def query_provider(name, context=None, registry=None):
-    """ Query content provider by `name`."""
+    """ Query content provider by `name` or by `name` and `context`.
+
+    By default, for lookup global registry will be used, but you can
+    provide your own registry you want to lookup from via `registry` keyword
+    argument.
+    """
     if registry is None:
         registry = getGlobalSiteManager()
     provider = registry.adapters.lookup(
@@ -19,7 +24,13 @@ def query_provider(name, context=None, registry=None):
 
 
 def get_provider(name, context=None, registry=None):
-    """ Get content provider by `name` or raise LookupError."""
+    """ Get content provider by `name` or by `name` and `context`.
+    If no providers was registered -- it will raise LookupError.
+
+    By default, for provider lookup global registry will be used, but you can
+    provide your own registry you want to lookup from via `registry` keyword
+    argument.
+    """
     provider = query_provider(name, context=context, registry=registry)
     if provider is None:
         raise LookupError("No provider was found for name '%s'" % name)
